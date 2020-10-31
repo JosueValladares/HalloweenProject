5b0e9b8965f2edaf9b72d79870a54c1c83c1d2a1
JosueValladares <jjvd023@gmail.com>
2020-10-31 12:44:14 -0400

    HalloweenTest was edited

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -2,4 +2,14 @@ public class HalloweenTest {
 
 // I am about to give hard time to everyone because I won't give them candy
 
+    //are we there yet?????
+    // no yet!!!!
+    //lets play a game
+    //it is fun to drive so far
+
+
+
+    //There will be no treat and trick this year
+    //Stay home and be safe or wear your mask outside
+
 }
